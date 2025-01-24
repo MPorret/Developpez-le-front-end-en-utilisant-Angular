504047bf902f74beb1e50a02ae2d8c424dab870e
Escanor1986 <lionel.zovi@gmail.com>
2025-01-24 17:06:00 +0100

    Improved error handling in olympics.service.ts & Added JSDoc for documentation + Begun with observable in home.component.ts

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -3,35 +3,38 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 
+// * JSDoc is a markup language used to annotate JavaScript code. It's used to document the codebase and provide additional information about the code.
+//! https://www.typescriptlang.org/docs/handbook/jsdoc-supported-types.html?utm_source=chatgpt.com
+
 /**
- * Service responsible for fetching and managing Olympic data.
+ * * Service responsible for fetching and managing Olympic data.
  */
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   /**
-   * URL to the Olympic data JSON file.
+   * * URL to the Olympic data JSON file.
    * @private
    */
   private readonly olympicUrl = './assets/mock/olympic.json';
 
   /**
-   * BehaviorSubject holding the current state of Olympic data.
+   * * BehaviorSubject holding the current state of Olympic data.
    * @private
    */
   private readonly olympics$ = new BehaviorSubject<any>(undefined);
 
   /**
-   * Constructs the OlympicService.
+   * * Constructs the OlympicService.
    * @param {HttpClient} http - The HTTP client used for making requests.
    */
   constructor(private readonly http: HttpClient) {}
 
   /**
-   * Loads the initial Olympic data from the JSON file.
-   * On success, updates the olympics$ BehaviorSubject with the fetched data.
-   * On error, logs the error and updates olympics$ with an error message.
+   * * Loads the initial Olympic data from the JSON file.
+   * * On success, updates the olympics$ BehaviorSubject with the fetched data.
+   * * On error, logs the error and updates olympics$ with an error message.
    * @returns {Observable<any>} An observable of the HTTP request.
    */
   loadInitialData(): Observable<any> {
@@ -42,7 +45,8 @@ export class OlympicService {
         console.error('Error loading Olympic data:', error);
 
         // Provide a user-friendly error message
-        const friendlyErrorMessage = 'An error occurred while loading the Olympic data.';
+        const friendlyErrorMessage =
+          'An error occurred while loading the Olympic data.';
         this.olympics$.next({ error: friendlyErrorMessage });
 
         // Return an observable with the user-friendly error message
@@ -52,7 +56,7 @@ export class OlympicService {
   }
 
   /**
-   * Returns an observable of the Olympic data.
+   * * Returns an observable of the Olympic data.
    * @returns {Observable<any>} An observable containing the Olympic data.
    */
   getOlympics(): Observable<any> {
