bf86e0ca2bfed3f1f75bd781157fe4eb503e404d
MarineP <marine.porret@gmail.com>
2025-02-11 12:51:43 +0100

    add data interface and clean data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, reduce } from 'rxjs';
+import { OlympicCountry } from 'src/app/core/models/Olympic';
+import { Participation } from 'src/app/core/models/Participation';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -9,10 +11,24 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
+  data: any = [];
 
-  constructor(private olympicService: OlympicService) {}
+  constructor(private olympicService: OlympicService) {
+  }
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    this.olympics$.subscribe( (value) => {
+      if (value) {
+        value.forEach((countryData: OlympicCountry) => {
+          let medals: number = 0;
+          countryData.participations.forEach((participation: Participation) => medals += participation.medalsCount);
+          this.data.push({
+            country: countryData.country,
+            medals: medals
+          });
+        })
+      }
+    });
   }
 }
