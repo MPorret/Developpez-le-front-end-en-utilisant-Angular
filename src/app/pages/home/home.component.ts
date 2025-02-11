5ac7331c5018187a87911ba4ff241b6797e0512e
MarineP <marine.porret@gmail.com>
2025-02-11 13:07:07 +0100

    update Angular version

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,9 +5,10 @@ import { Participation } from 'src/app/core/models/Participation';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+    standalone: false
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
