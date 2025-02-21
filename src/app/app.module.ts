d2c354c9c64deb2d73f044f97ab5c4784afcfd17
MarineP <marine.porret@gmail.com>
2025-02-21 11:32:57 +0100

    Clean code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderPageComponent } from "./core/shared/header-page/header-page.component";
 import { ButtonComponent } from "./core/shared/button/button.component";
 import { LoadingIndicatorComponent } from "./core/shared/loading-indicator/loading-indicator.component";
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule,
     HomeComponent,
     HeaderPageComponent,
-    NotFoundComponent,
     AppRoutingModule,
     ButtonComponent,
     HttpClientModule,
+    NotFoundComponent,
     NgxChartsModule,
     BrowserAnimationsModule, LoadingIndicatorComponent],
   providers: [
--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -4,7 +4,7 @@ import { HeaderPageComponent } from "../../core/shared/header-page/header-page.c
 import { Indicator } from 'src/app/core/models/Indicator';
 import { ButtonComponent } from "../../core/shared/button/button.component";
 import { OlympicService } from 'src/app/core/services/olympic.service';
-import { Subscription } from 'rxjs';
+import { Subscription, tap } from 'rxjs';
 import { OlympicCountry } from 'src/app/core/models/Olympic';
 import { DataItem, Series } from '@swimlane/ngx-charts';
 import { Participation } from 'src/app/core/models/Participation';
@@ -29,46 +29,53 @@ export class DetailsComponent implements OnInit, OnDestroy{
 
   ngOnInit(): void {
     this.loadingService.loadingOn();
-    this.subscription = this.olympicService.getOlympics().subscribe((value)=> {
-      if (Array.isArray(value)){
-        const countrySelected = value.find((e: OlympicCountry)=> e.country === this.countryName);
-        if (countrySelected) {
-          const {participations, country} = countrySelected;
-          const calculTotal = (data: Participation[], key: 'medalsCount' | 'athleteCount') => {
-            let result = 0;
-            data.forEach((value: Participation) => {
-              result += value[key];
-            })
-            return result;
+    this.subscription = this.olympicService.getOlympics().pipe(
+      tap((value)=> {
+        if (Array.isArray(value)){
+          const selectedCountry = value.find((e: OlympicCountry)=> e.country === this.countryName);
+          if (selectedCountry) {
+            this.formatDataForLineChart(selectedCountry)
+            this.loadingService.loadingOff();
+          } else {
+            this.error = 404;
           }
-        this.indicators.push({label: "Number of entries", value: participations.length});
-        this.indicators.push({label: "Total number medals", value: calculTotal(participations, 'medalsCount')});
-        this.indicators.push({label: "Total number of athletes", value: calculTotal(participations, 'athleteCount')});
-
-        const series: DataItem[] = [];
-        participations.forEach((participation: Participation) => {
-          series.push({
-            "name": participation.year.toString(),
-            "value": participation.medalsCount
-          })
-        })
-
-        this.data = [{
-          "name": country,
-          "series": series,
-        }];
-        this.loadingService.loadingOff();
-        } else {
-          this.error = 404;
+        } else if (value) {
+          this.error = value;
         }
-      } else if (value) {
-        this.error = value;
-      }
-    })
+        this.loadingService.loadingOff();
+      }),
+
+    ).subscribe()
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe()
-  }
+    }
 
+  formatDataForLineChart(selectedCountry: OlympicCountry){
+    const {participations, country} = selectedCountry;
+    const calculTotal = (data: Participation[], key: 'medalsCount' | 'athleteCount') => {
+      let result = 0;
+      data.forEach((value: Participation) => {
+        result += value[key];
+      })
+      return result;
+    }
+    this.indicators.push({label: "Number of entries", value: participations.length});
+    this.indicators.push({label: "Total number medals", value: calculTotal(participations, 'medalsCount')});
+    this.indicators.push({label: "Total number of athletes", value: calculTotal(participations, 'athleteCount')});
+
+    const series: DataItem[] = [];
+    participations.forEach((participation: Participation) => {
+      series.push({
+        "name": participation.year.toString(),
+        "value": participation.medalsCount
+      })
+    })
+
+    this.data = [{
+      "name": country,
+      "series": series,
+    }];
+  }
 }
